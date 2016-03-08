1ec372b4c3d6b3625c426d9579be211ad0dce696
Willem Victor <wimpievictor@gmail.com>
2016-03-08 19:56:04 +0200

    Create basic vm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 
 
-int main() {
+#include "../lib/wren/src/include/wren.h"
+
+
+int main(void) {
+
+    WrenConfiguration config;
+    wrenInitConfiguration(&config);
+    WrenVM* vm = wrenNewVM(&config);
+
+    wrenInterpret(vm, "System.print(\"Hello World\")");
+
+    wrenFreeVM(vm);
+
     return 0;
 }
